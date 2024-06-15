4903c463f8142c587c117d1ee915a269fbf2d05b
Nikunj Jadav <dirensj@gmail.com>
2024-06-15 21:46:42 +0530

    Tailwind CSS Installed and Added Tailwind classes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-3xl font-bold underline text-[#0e7490] ">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
