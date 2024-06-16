19a651858177f5a7a67580f9e19cc2316585c297
Nikunj Jadav <dirensj@gmail.com>
2024-06-16 10:38:36 +0530

    User Update and Delete API coded

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -32,3 +32,54 @@ export const getAll = async(req,res)=>{
         res.status(500).json({error:error});
     }
 }
+
+/* Get One User */
+
+export const getOne = async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const userExist = await UserModels.findById(id);
+        if(!userExist){
+            return res.status(404).json({msg:"User not found."});
+        }
+        res.status(200).json(userExist);
+
+    }catch(error){
+        res.status(500).json({error:error});
+    }
+}
+
+/* Update an user */
+
+export const updateUser = async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const userExist = await UserModels.findById(id);
+        if(!userExist){
+            return res.status(404).json({msg:"User not found."});
+        }
+        const updatedData = await UserModels.findByIdAndUpdate(id,req.body,{new:true});
+        res.status(200).json(updatedData);
+
+    }catch(error){
+        res.status(500).json({error:error});
+    }
+}
+
+
+/* Delete an user */
+
+export const deleteUser = async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const userExist = await UserModels.findById(id);
+        if(!userExist){
+            return res.status(404).json({msg:"User not found."});
+        }
+        await UserModels.findByIdAndDelete(id);
+        res.status(200).json("{msg:User Deleted Successfully.}");
+
+    }catch(error){
+        res.status(500).json({error:error});
+    }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import {create,getAll} from "../controller/userController.js";
+import {create,getAll,getOne,updateUser,deleteUser} from "../controller/userController.js";
 const route = express.Router();
 
 route.post("/create",create);
 route.get("/all",getAll);
+route.get("/getone/:id",getOne);
+route.put("/update/:id",updateUser);
+route.delete("/delete/:id",deleteUser);
 
 export default route;
